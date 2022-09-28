e5e7e31452241148599b04b05a1e2b27c46b7832
CyprienF <floquet.cyprien@gmail.com>
2022-09-28 22:42:59 +0200

    chore: add prettier

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,7 @@
 <template>
-  <div>
-    HomeView
-  </div>
+  <div>HomeView</div>
 </template>
 
-<script setup lang="ts">
+<script setup lang="ts"></script>
 
-</script>
-
-<style scoped>
-
-</style>
+<style scoped></style>
